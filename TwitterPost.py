55e00bbebfb998f957acd48c36e8de7e29c99052
sureshsly <63937504+sureshsly@users.noreply.github.com>
2020-05-05 17:07:30 +0530

    Update TwitterPost.py

--- TwitterPost.py
+++ TwitterPost.py
@@ -35,7 +35,6 @@ def post_master():
         post_status = random.choice(lines)
         # update the status  
         api.update_status(status=post_status)
-        lines.close()
     except Exception as errmsg:
         telmsg(str(errmsg))
     finally:
@@ -47,7 +46,7 @@ def main():
     while True:
         _currentTime = time.localtime()
         c_time = time.strftime("%H:%M:%S", _currentTime)
-        if (c_time == '11:35:00') or (c_time == '11:45:00') or (c_time == '11:55:00'):
+        if (c_time == '12:45:00') or (c_time == '11:45:00') or (c_time == '13:55:00'):
             post_master()
         time.sleep(1)
 
